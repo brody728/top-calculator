d613227ff02ccdf00a3b7051373f04e1d677fe82
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-13 19:38:56 -0700

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(x, y, operation) {
+    switch (operation) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '×':
+            return multiply(x, y);
+        case '÷':
+            return divide(x, y);
+    }
+}
